5fce0e1c770a4f1a41935cff777379a9dd78f178
James Thompson <cthompson88@ymail.com>
2018-09-22 17:35:46 +1200

    Add project files.

---
+++ ResturantBooking/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ResturantBooking.Startup))]
+namespace ResturantBooking
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
